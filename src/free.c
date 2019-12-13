f6638c330c8db758d4a2a4e0d3bb437fa80a9e90
Michael HALIT <mhalit@e1r9p14.42.fr>
2019-12-13 16:41:05 +0100

    add munmap

--- src/free.c
+++ src/free.c
@@ -19,10 +19,21 @@ void		double_free_error(void *ptr)
 	return ;
 }
 
+void		unmap(t_pagezone *current)
+{
+	munmap(current->map, current->available);
+}
+
 void		increase_pagezone(t_pagezone *current, size_t n)
 {
 	current->available += n;
 	current->used -= n;
+	if (current->type == TINY && current->used == 0 && g_store.nb_tiny > 1)
+		unmap(current);
+	if (current->type == MEDIUM && current->used == 0 && g_store.nb_medium > 1)
+		unmap(current);
+	if (current->type == LARGE && current->used == 0 && g_store.nb_large > 1)
+		unmap(current);
 }
 
 void		free(void *ptr)
